f092b781aaae7e69faae9718a4608f0fd289f238
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-06 10:28:37 +0100

    💄 update db

--- person/models.py
+++ person/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Person(AbstractUser):
+    cin=models.CharField(primary_key=True,max_length=8)
+    username=models.CharField("Username" , max_length=50 , unique=True)
+    email=models.EmailField("Email" ,unique=True)
+    USERNAME_FIELD="username"
+
+
+    def __str__(self):
+        return self.username
