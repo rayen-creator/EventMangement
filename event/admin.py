b99431b738c6d471883c809f083221e1a905f2e4
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-13 09:35:23 +0100

    ✅ update list_display

--- event/admin.py
+++ event/admin.py
@@ -8,9 +8,16 @@ from .models import Event, Participants
 class EventAdmin(admin.ModelAdmin):
     list_display=(
         'title','description','image','categoy','state',
-        'nbr_participant','evt_date','created_date','updated_date','participant','organisateur'
+        'nbr_participant','evt_date','created_date','updated_date','e','organisateur'
     )
+    list_per_page=2
+    ordering=['title']
     # list_display=[field.name for field in Event._meta.get_fields()]
+    def e(self,obj):
+        count=obj.participant.count()
+        return count
+    e.short_description='nombre participant'
+
     def participant(self):
         return "\n".join([p.participants for p in self.participant.all()])
 admin.site.register(Participants)
