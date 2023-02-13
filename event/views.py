5fa1194f6a4cf9875920bba8d7da424e90573ca8
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-13 11:21:41 +0100

    ✨ added new view event

--- event/views.py
+++ event/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.http.response import HttpResponse
+from .models import Event
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Rendering string content")
+
+def list_event(request):
+    list=Event.objects.all()
+    return render(request, 'event/list_events.html', {'list':list})
