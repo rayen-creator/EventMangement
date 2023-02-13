69565b4e9de8bcc6e0381d8fa56163f84ef0fbd0
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-13 11:21:22 +0100

    ✨ added few features

--- event/admin.py
+++ event/admin.py
@@ -1,8 +1,32 @@
-from django.contrib import admin
+from django.contrib import admin , messages
 from .models import Event, Participants
 # Register your models here.
 
-# admin.site.register(Event)
+
+
+class ParticipantsInline(admin.TabularInline):
+    model = Participants
+    # readonly_fields=('date_creation',)
+    extra=1
+    can_delete=True
+    classes=['collapse']
+
+def set_Refuse(ModelAdmin,request,queryset):
+    rows_updated=queryset.update(state=False)
+    if (rows_updated==1):
+        message_bit="1 event was"
+    else:
+        message_bit=f"{rows_updated} events were"
+    messages.success(request,f'{message_bit} successfully updated !')
+
+def set_Accept(ModelAdmin,request,queryset):
+    rows_updated=queryset.update(state=True)
+    if (rows_updated==1):
+        message_bit="1 event was"
+    else:
+        message_bit=f"{rows_updated} events were"
+    messages.success(request ,f'{message_bit} successfully updated !')
+
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
@@ -10,14 +34,34 @@ class EventAdmin(admin.ModelAdmin):
         'title','description','image','categoy','state',
         'nbr_participant','evt_date','created_date','updated_date','e','organisateur'
     )
+
+    list_filter=('title','categoy','state','organisateur')
+
+    actions=[set_Accept,set_Refuse]
+    
+    actions_on_bottom=True
+    
+    autocomplete_fields=['organisateur']
+
+    inlines=[ParticipantsInline]
+
+    readonly_fields=('created_date','updated_date')
+
+    fieldsets=(
+        ('Event Information',{
+            'fields':('title','description','image','categoy','state','nbr_participant','organisateur')
+        })   ,
+        ('Dates' ,{
+            'fields':('evt_date','created_date','updated_date')
+        }) 
+    )
+
     list_per_page=2
     ordering=['title'] 
-    # list_display=[field.name for field in Event._meta.get_fields()]
+
     def e(self,obj):
         count=obj.participant.count()
         return count
     e.short_description='nombre participant'
 
-    def participant(self):
-        return "\n".join([p.participants for p in self.participant.all()])
 admin.site.register(Participants)
