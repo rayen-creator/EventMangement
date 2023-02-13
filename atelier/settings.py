359b3c63173d644c84b85f547fbbf4eca4890a8b
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-13 11:20:58 +0100

    🚑 update main routes

--- atelier/settings.py
+++ atelier/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'atelier.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
