2af443cfd6c606975008fc3e971320103a77085c
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-20 09:48:44 +0100

    ✅ added new path with parameter

--- event/views.py
+++ event/views.py
@@ -4,8 +4,8 @@ from django.http.response import HttpResponse
 from .models import Event
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Rendering string content")
+def index(request,name):
+    return HttpResponse("Rendering "+name)
 
 def list_event(request):
     list=Event.objects.all()
