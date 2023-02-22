ac746d94791579891bffb8c84eca7a59809ab606
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-22 23:15:34 +0100

    ✅ update urls

--- event/urls.py
+++ event/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('<str:name>', index),
+    # path('<str:name>', index),
     # path('list/',list_event),
     path('affiche/', ListEvents.as_view(), name="Affiche"),
     path('add/', AddEvent.as_view(), name="add"),
